1bf7bac01a6df3f0af30ed07c9b8d589130f4cd2
Steffen Winde <inserv@winde-ganzig.de>
2023-03-14 10:30:45 +0100

    add theme

---
+++ 2023-03-14-chancelog.md
@@ -0,0 +1,29 @@
+---
+title: CHANCELOG
+issue: 
+author: Steffen Winde
+author_email: inserv@winde-ganzig.de
+---
+
+# Core
+
+*
+
+___
+
+# API
+
+*
+
+___
+
+# Administration
+
+*
+
+___
+# Storefront
+*
+___
+# Upgrade Information
+
---
+++ src/BeesKraeuterSeifertTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace BeesKraeuterSeifertTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class BeesKraeuterSeifertTheme extends Plugin implements ThemeInterface
+{
+}
